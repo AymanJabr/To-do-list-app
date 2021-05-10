f8a483ec90f5e941e35b727f63556b7073301a68
ayman <aymanjaber2012@gmail.com>
2021-05-10 21:51:38 +0300

    Expand single todo to see details after updating

--- src/clicking-events.js
+++ src/clicking-events.js
@@ -6,68 +6,92 @@ import insertAllProjects from "./dom-manipulation";
 import insertModal from "./modal";
 
 let clickingEvents = () => {
-  // Wait for Create Project Button to be clicked
-  let create_project_button = document.getElementsByClassName(
-    "add-project-button"
-  )[0];
-  create_project_button.onclick = () => {
-    let title = document.getElementById("new-project-title").value;
-    if (title != "") {
-      let new_project = createProject(title);
-      app.addProject(new_project);
-      insertAllProjects();
-    }
-  };
-
-  // Wait for Particular Project Div to be clicked on
-  let project_divs = document.querySelectorAll(".project-title");
-  project_divs.forEach((div, index) => {
-    div.onclick = () => {
-      let todos_div = document.querySelectorAll(`.project-${index}-todos`)[0];
-      todos_div.style.display =
-        todos_div.style.display == "none" ? "block" : "none";
+    // Wait for Create Project Button to be clicked
+    let create_project_button = document.getElementsByClassName(
+        "add-project-button"
+    )[0];
+    create_project_button.onclick = () => {
+        let title = document.getElementById("new-project-title").value;
+        if (title != "") {
+            let new_project = createProject(title);
+            app.addProject(new_project);
+            insertAllProjects();
+        }
     };
-  });
 
-  // Wait for submit new todo modal button clicked
-  let submit_buttons = document.querySelectorAll(".submit-new-todo");
-  submit_buttons.forEach((button) => {
-    button.onclick = () => {
-      let title = document.getElementById("todo-title").value;
-      let description = document.getElementById("todo-desc").value;
-      let dueDate = document.getElementById("todo-due-date").value;
-      let priority = document.getElementById("todo-priority").value;
-      let todo = createTodo(title, description, dueDate, priority);
-      app.getProjects()[button.id].addTodo(todo);
+    // Wait for Particular Project Div to be clicked on
+    let project_divs = document.querySelectorAll(".project-title");
+    project_divs.forEach((div, index) => {
+        div.onclick = () => {
+            let todos_div = document.querySelectorAll(`.project-${index}-todos`)[0];
+            todos_div.style.display =
+                todos_div.style.display == "none" ? "block" : "none";
+        };
+    });
 
-      document.getElementById("newTodoModal").style.display = "none";
-      insertAllProjects();
-      document.getElementsByClassName(
-        `project-${button.id}-todos`
-      )[0].style.display = "block";
-    };
-  });
+    // Wait for submit new todo modal button clicked
+    let submit_buttons = document.querySelectorAll(".submit-new-todo");
+    submit_buttons.forEach((button) => {
+        button.onclick = () => {
+            let title = document.getElementById("todo-title").value;
+            let description = document.getElementById("todo-desc").value;
+            let dueDate = document.getElementById("todo-due-date").value;
+            let priority = document.getElementById("todo-priority").value;
+            let todo = createTodo(title, description, dueDate, priority);
+            app.getProjects()[button.id].addTodo(todo);
+
+            document.getElementById("newTodoModal").style.display = "none";
+            insertAllProjects();
+            document.getElementsByClassName(
+                `project-${button.id}-todos`
+            )[0].style.display = "block";
+        };
+    });
 
 
     let all_nodes = document.querySelectorAll(".todo-completed-button")
     let my_array = [...all_nodes]
-    my_array.forEach( button => {
+    my_array.forEach(button => {
         button.addEventListener('click', () => {
             // console.log("clicked on button")
 
-            button.parentElement.parentElement.classList.add("todo-completed")
+            button.parentElement.parentElement.classList.toggle("todo-completed")
             let myProjectTitle = button.parentElement.parentElement.parentElement.parentElement.firstChild.firstChild.innerText
-            let myTodoTitle = (button.parentElement.parentElement.childNodes[1].innerHTML)
+            let myTodoTitle = button.parentElement.parentElement.childNodes[1].innerHTML
             let myProject = app.getProjectByTitle(myProjectTitle)
-            let myTodo =  myProject.getTodoByTitle(myTodoTitle)
+            let myTodo = myProject.getTodoByTitle(myTodoTitle)
             myTodo.toggleStatus()
 
-            insertAllProjects();
+
+            // console.log(myProject)
+
+            insertAllProjects()
 
 
-            let myParent = button.parentElement.parentElement.parentElement.parentElement.childNodes[2]
-            console.log(myParent)
-            myParent.style.display = "block";
+            // let myParent = button.parentElement.parentElement.parentElement.parentElement.childNodes[2]
+            // console.log(myParent)
+            // myParent.style.display = "block";
+
+
+             document.querySelectorAll('.project-todos > .todo > .todo-title').forEach(project => {
+                 let myParent = project.parentElement.parentElement
+                // console.log(project.innerText)
+                if (project.innerText == myTodoTitle) {
+                    // myParent.classList.toggle('todo-completed')
+                    // console.log(myParent)
+                    myParent.classList.add("display-block")
+
+                }
+
+            })
+            
+           
+
+
+            // await addAllProjects.then(() => {
+
+            // })
+
 
             // Wait for Particular Project Div to be clicked on
             // let project_divs = document.querySelectorAll(".project-title");
@@ -101,7 +125,7 @@ let clickingEvents = () => {
 
 
 
-  insertModal();
+    insertModal();
 };
 
 export default clickingEvents;
