237c523538e6d177fbac589556aab8b40dfdc447
ayman <aymanjaber2012@gmail.com>
2021-05-12 11:58:31 +0300

    add todo-model tests

--- src/todo-model.test.js
+++ src/todo-model.test.js
@@ -4,14 +4,30 @@ let newTodo = createTodo(
   "New Todo Name",
   "New Todo Description",
   "2021-12-20",
-  0
+  0,
+  true
 );
 
-test("Create Todo Object Successfully", () => {
-  
-  expect(Object.is(newTodo, createTodo)).toBe(true);
-});
-
-test("Create Todo Object Successfully", () => {
+test("Created Todo saves name correctly", () => {
   expect(newTodo.getTitle()).toBe("New Todo Name");
+});
+test("Created Todo saves description correctly", () => {
+  expect(newTodo.getDescription()).toBe("New Todo Description");
+});
+test("Created Todo saves due-date correctly", () => {
+  expect(newTodo.getDueDate()).toBe("Completed!");
+});
+test("Created Todo saves priority correctly", () => {
+  expect(newTodo.getPriority()).toBe(0);
+});
+test("Created Todo saves status correctly", () => {
+  expect(newTodo.getStatus()).toBe(true);
+});
+test("Toggle Status returns false", () => {
+  newTodo.toggleStatus()
+  expect(newTodo.getStatus()).toBe(false);
+});
+test("Change priority to 1", () => {
+  newTodo.changePriority(1)
+  expect(newTodo.getPriority()).toBe(1);
 });
