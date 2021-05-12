318c648dd25c680328343904c166b8f40dc8b33c
Zil Norvilis <zilasino27@gmail.com>
2021-05-12 11:37:50 +0200

    Add test for project model

---
+++ src/project-model.test.js
@@ -0,0 +1,24 @@
+const createProject = require("./project-model");
+const createTodo = require("./todo-model");
+let newTodo = createTodo(
+  "New Todo Name",
+  "New Todo Description",
+  "2021-12-20",
+  0,
+  true
+);
+let myProject = createProject("New Project Title");
+test("Project is correctly created with expected name", () => {
+  expect(myProject.getTitle()).toStrictEqual("New Project Title");
+});
+test("Todo Is correctly added to myProject", () => {
+  myProject.addTodo(newTodo);
+  expect(myProject.getTodos()).toStrictEqual([newTodo]);
+});
+test("Get todo by title correctly", () => {
+  expect(myProject.getTodoByTitle("New Todo Name")).toStrictEqual(newTodo);
+});
+test("Todo is correctly removed from Todos array", () => {
+  myProject.removeTodoByTitle("New Todo Name");
+  expect(myProject.getTodos()).toStrictEqual([]);
+});
