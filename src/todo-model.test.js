b784d3b39cd45052bca8f3f20a0e506739e6cbd7
ayman <aymanjaber2012@gmail.com>
2021-05-12 11:14:17 +0300

    Modify todo tests

--- src/todo-model.test.js
+++ src/todo-model.test.js
@@ -1,11 +1,17 @@
-import createTodo from "./todo-model";
+const createTodo = require('./todo-model')
+
+let newTodo = createTodo(
+  "New Todo Name",
+  "New Todo Description",
+  "2021-12-20",
+  0
+);
 
 test("Create Todo Object Successfully", () => {
-  let new_todo = createTodo(
-    "New Todo Name",
-    "New Todo Description",
-    "2021-12-20",
-    0
-  );
-  expect(new_todo.getTitle()).toBe("New Todo Name");
+  
+  expect(Object.is(newTodo, createTodo)).toBe(true);
+});
+
+test("Create Todo Object Successfully", () => {
+  expect(newTodo.getTitle()).toBe("New Todo Name");
 });
